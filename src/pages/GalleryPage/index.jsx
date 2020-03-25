a5b23eb5fad18a0fa8f2ba5cb82a473aca3f7eea
araujo <tiago18.araujoo@gmail.com>
2020-03-25 12:11:17 +0000

    gallery page link insertion correction

--- src/pages/GalleryPage/index.jsx
+++ src/pages/GalleryPage/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const GalleryPage = () => {
   return (
     <div>
       <h1>Gallery Page</h1>
+
+      <Link to="/about">
+        Go to about page.
+      </Link>
     </div>
   )
 };
